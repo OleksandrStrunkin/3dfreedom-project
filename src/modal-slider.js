d4cfa4ed532780ff4fd27b1644bd2dc5e0b9695f
VIper17052001 <bigjenya11@gmail.com>
2022-09-10 21:28:49 +0300

    update

--- src/modal-slider.js
+++ src/modal-slider.js
@@ -1,6 +1,5 @@
 var modal = document.getElementById('myModal');
 
-// Get the image and insert it inside the modal - use its "alt" text as a caption
 var image = document.querySelectorAll('.morephoto');
 var modalImg = document.getElementById('img01');
 var captionText = document.getElementById('caption');
@@ -12,10 +11,8 @@ image.forEach(Element => {
   });
 });
 
-// Get the <span> element that closes the modal
 var span = document.getElementsByClassName('close')[0];
 
-// When the user clicks on <span> (x), close the modal
 span.onclick = function () {
   modal.style.display = 'none';
 };
