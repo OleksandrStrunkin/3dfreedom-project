8bdefa7bd14ec7666703e09b49b24ce89189a7cb
OleksandrStrunkin <108118341+OleksandrStrunkin@users.noreply.github.com>
2022-09-13 13:47:33 +0000

    Deploying to gh-pages from @ OleksandrStrunkin/3dfreedom-project@f7dfad2970313057d778b9a71ab7849bf3994f3a 🚀

--- index.a1b39f84.js
+++
@@ -1,2 +0,0 @@
-var modal=document.getElementById("myModal"),image=document.querySelectorAll(".more-photo__img"),modalImg=document.getElementById("img01"),captionText=document.getElementById("caption");image.forEach((function(e){e.addEventListener("click",(function(){modal.style.display="block",modalImg.src=e.src,captionText.innerHTML=e.alt}))}));var span=document.getElementsByClassName("button__close")[0];span.onclick=function(){modal.style.display="none"};
-//# sourceMappingURL=index.a1b39f84.js.map
